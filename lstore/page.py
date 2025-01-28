6097c7d881ae95ad272f2dde96d4f035fcfc2d63
luukjanssen9 <luukjanssen9@outlook.com>
2025-01-28 10:49:10 -0800

    has_capacity and write in Pages

--- lstore/page.py
+++ lstore/page.py
@@ -6,9 +6,17 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
-        pass
+        if self.data.length < 4096:
+            return True
+        return False
+        
 
     def write(self, value):
         self.num_records += 1
-        pass
+        # append values until page is full
+        if self.has_capacity() is True:
+            self.data.append(value)
+        else:
+            print("error: page is full")
+        return
 
