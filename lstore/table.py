7725e741e3e13a3406c882b4908e3f4eda7f64bc
luukjanssen9 <luukjanssen9@outlook.com>
2025-02-03 14:54:57 -0800

    better outline

--- lstore/table.py
+++ lstore/table.py
@@ -1,5 +1,6 @@
 from lstore.index import Index
 from time import time
+from lstore.page import Page
 
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
@@ -20,13 +21,17 @@ class Table:
     :param name: string         #Table name
     :param num_columns: int     #Number of Columns: all columns are integer
     :param key: int             #Index of table key in columns
+    :param page_directory: dict #Dictionary of pages, returns page location of record given rid
+    :param index: Index         #Index object for the table
+    :param pages: list          #List of pages in the table
     """
     def __init__(self, name, num_columns, key):
-        self.name = name
+        self.name = name 
         self.key = key
         self.num_columns = num_columns
         self.page_directory = {}
         self.index = Index(self)
+        self.pages = []
         pass
 
     def __merge(self):
@@ -35,12 +40,16 @@ class Table:
 
     def __insert(self, record):
         #TODO: find an available page or create a new one
-
+        #__findAvailablePageOrCreateNewOne
+                
         #TODO: write record to page
 
         #TODO: update page directory
-        self.page_directory[record.rid] = record
-        
+
+        #TODO: update index
+        pass
+
+    def __findAvailablePageOrCreateNewOne():
         pass
 
     def __delete(self, key):
