a0217448dc5e6ea83d6b458c9b44a17f9db3ab9d
RamiAbudamous <rami.abudamous@gmail.com>
2025-02-11 11:09:16 -0800

    cleaned up the delete code

--- lstore/query.py
+++ lstore/query.py
@@ -23,9 +23,6 @@ class Query:
     # Return False if record doesn't exist or is locked due to 2PL
     """
     def delete(self, primary_key):
-
-        # print(f"deleting primary key #{primary_key}")
-
         # loop through all records to find the correct one
         for page_range in self.table.page_ranges:
             for base_page in page_range.base_pages:
@@ -38,40 +35,26 @@ class Query:
                             # Follow indirection pointer to get the latest tail page version
                             current_rid = base_page.pages[config.INDIRECTION_COLUMN].read(record_number) 
                             
-                            # Only delete the base page's RID after copying it, as it is needed to delete tail page records
-                            # it seems like we need to delete the primary key not the RID, as the key is used when selecting.
+                            # then delete the RID and timestamp
                             base_page.pages[config.RID_COLUMN].write(0, record_number) # set base page RID to 0
-                            # base_page.pages[config.PRIMARY_KEY_COLUMN].write(0, record_number) # set base page Primary Key to 0
-                            
-                            # print(f"new RID is {base_page.pages[config.RID_COLUMN].read(record_number)}")
+                            base_page.pages[config.TIMESTAMP_COLUMN].write(0, record_number) # set the timestamp to 0 as well
 
                             while current_rid != 0 and current_rid in self.table.page_directory:
                                 # use the RID to get the next tail page and record
                                 tail_page_range, tail_base_page, tail_record_num = self.table.page_directory[current_rid]
                                 tail_page = self.table.page_ranges[tail_page_range].tail_pages[tail_base_page]
 
-                                # if we want to delete all tail page records along the way.
-                                # otherwise move the next line to outside the while loop, so that only the last instance is removed
+                                # to delete all tail page records along the way.
                                 tail_page.pages[config.RID_COLUMN].write(0, tail_record_num)
-
-                                # same as above, set primary key to 0, not RID
-                                # tail_page.pages[config.PRIMARY_KEY_COLUMN].write(0, tail_record_num)
+                                tail_page.pages[config.TIMESTAMP_COLUMN].write(0, tail_record_num)
 
                                 current_rid = tail_page.pages[config.INDIRECTION_COLUMN].read(tail_record_num)  # Move to the next older version
                             
-                            # successfully deleted
-                            return True
-                            
-                # very inefficient, do not use unless absolutely necessary
-                # for tail_page in page_range.tail_pages:
-                #     for record_number in range(0, tail_page.pages.num_records):
-                #         if tail_page.page[4].read(record_number) == primary_key: #5th column is the primary key
-                #             tail_page.page[0].write(0, record_number) # set rid to 0
-
-            
+                            return True            
             # if we reach here, the record does not exist
             return False
     
+    # is this function ever used? we might be able to remove it.
     def get_vals(self, rid):
         if rid in self.table.page_directory:
             return self.table.page_directory[rid]
