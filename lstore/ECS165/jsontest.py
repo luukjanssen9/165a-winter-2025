932e226b92875764945f24f231986dba7a969c55
RamiAbudamous <rami.abudamous@gmail.com>
2025-02-23 15:13:44 -0800

    made reversible json input and outputs in a test script

--- lstore/ECS165/jsontest.py
+++ lstore/ECS165/jsontest.py
@@ -1,12 +1,19 @@
 import json
 import os
 
-currdir = "."
-for thing in os.listdir(currdir):
-    print(f"{thing}: {os.path.isdir(thing)}")
+# THIS SEGMENT TESTS TO SEE WHAT IN THE DIRECTORY IS A FOLDER VS A FILE
+# TRUE = FOLDER, FAKSE = FILE
+# currdir = "."
+# for thing in os.listdir(currdir):
+#     print(f"{thing}: {os.path.isdir(thing)}")
 
-path = "Grades"
-with open(f'{path}.json', 'r') as table_metadata:
+# YOU CAN TEST THE SYSTEM BY SWITCHING PATH1 AND PATH2
+# IF IT WORKS, THEN THE CODE SHOULD RUN TO COMPLETION AND COPY ONE OUTPUT TO THE OTHER
+# THEN YOU CAN SWAP THE FILE NAMES AND HAVE IT GO THE OTHER WAY
+# YOU CAN ALSO EDIT A VALUE IN ONE FILE AND WATCH IT CHANGE IN THE OTHER FILE
+path2 = "output"
+path1 = "Grades"
+with open(f'{path1}.json', 'r') as table_metadata:
     data = json.load(table_metadata)
 
 key = data["key"]
@@ -15,8 +22,23 @@ json_page_dir = data["page_directory"]
 
 page_directory = {}
 for i in json_page_dir:
-    page_directory[i] = (json_page_dir[i]['page_range'], json_page_dir[i]['base_page'], json_page_dir[i]['record_number'])
-            
+    page_directory[i] = {
+        "page_range" : json_page_dir[i]['page_range'],
+        "base_page" : json_page_dir[i]['base_page'],
+        "record_number" : json_page_dir[i]['record_number']
+    }
 
-print(f"key is {key}, there are {num_columns} columns")
-print(page_directory)
+# print(f"key is {key}, there are {num_columns} columns")
+# print(page_directory)
+
+# OUTPUT TO JSON
+output = {
+    "key" : key,
+    "num_columns" : num_columns,
+    "page_directory" : page_directory
+}
+
+outputjson = json.dumps(output, indent=4)
+# print(f"output is {outputjson}")
+with open(f'{path2}.json', 'w') as json_output_file:
+    json_output_file.write(outputjson)
