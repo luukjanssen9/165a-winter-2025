bb3cd511f63e2784a85563fcd66801ede73158e4
luukjanssen9 <luukjanssen9@outlook.com>
2025-02-25 18:26:06 -0800

    fixed double inserting page range

--- lstore/query.py
+++ lstore/query.py
@@ -95,22 +95,13 @@ class Query:
             new_page_range = pageRange(self.table.num_columns, latest_bp=0, latest_tp=None, new=True)
             self.table.page_ranges.append(new_page_range)
             open_page_range_number = 0
-
-
+            # TODO: do we have to update new to FALSE?
         open_base_page_number = self.table.page_ranges[open_page_range_number].latest_base_page
-        
         open_record_number  = self.table.page_ranges[open_page_range_number].base_pages[open_base_page_number].latest_record_number
         
         # Update page directory for hash table
         self.table.page_directory[rid] = (open_page_range_number, open_base_page_number, open_record_number)
-
-        # If open_base_page_number = 0 and open_record_number = 0, we need to create a new page range
-        if open_base_page_number == 0 and open_record_number == 0 and open_page_range_number is not None:
-            # Create a new page range
-            new_page_range = pageRange(self.table.num_columns, latest_bp=0, latest_tp=None, new=True)
-            self.table.page_ranges.append(new_page_range)
-            self.table.latest_page_range = len(self.table.page_ranges) - 1
-            # TODO: do we have to update new to FALSE?
+            
 
         # Get the latestpage (which should have capacity if we are doing our check at the end right)
         for column_number in range(self.table.num_columns):
